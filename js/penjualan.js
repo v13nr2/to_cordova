3fdecb8286e778c17c930a2ce6e688902b2f84ef
v13nr2 <nanang.programmer@gmail.com>
2019-12-13 14:41:56 +0700

    reload total qty

--- js/penjualan.js
+++ js/penjualan.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
 
 var barisCounter = 1;
 var set_TR_number = 0;
+var totalQTY = 0;
 
 function signout(){
 	window.location.href = "../../pages/examples/sign-in.html";
@@ -28,15 +29,16 @@ function addTR(){
 	barisCounter++;
 	TR = '<tr id="tr_ke_'+barisCounter+'"><td><input type="text" id="produk_'+barisCounter+'" size="8"  value="Produk '+barisCounter+'" onfocus="set_TR('+barisCounter+')"  ></td>'+
 			'<td><input type="text" id="harga_'+barisCounter+'" size="5" oninput="hitung('+barisCounter+')" value="0" onfocus="set_TR('+barisCounter+')"  ></td>'+
-			'<td><input type="text" id="qty_'+barisCounter+'"  size="3"  oninput="hitung('+barisCounter+')" value="1" onfocus="set_TR('+barisCounter+')"  ></td>'+
+			'<td><input type="text" id="qty_'+barisCounter+'"  size="3"  oninput="hitung('+barisCounter+'); reloadTotalQTY()" value="1" onfocus="set_TR('+barisCounter+')"  ></td>'+
 			'<td><input type="text" id="total_'+barisCounter+'"  size="5" readonly value="0" onfocus="set_TR('+barisCounter+')"  ></td></tr>';
 		
 	$("#mainTable").append(TR);
-	$("#totalqty").html(($("#totalqty").val()*1)+1);
+	reloadTotalQTY();
 }
 
 function removeTR(z){
 	$("#tr_ke_"+TR_number).remove();
+	reloadTotalQTY();
 }
 
 function hitung(z){
@@ -54,4 +56,15 @@ function goto_penjualan(){
 
 function set_TR(z){
 	TR_number = z;
+}
+
+function reloadTotalQTY(){
+	totalQTY = 0;
+	for(i=0;i<=400;i++){
+		if($('#qty_'+i).val()!=undefined){
+			totalQTY = totalQTY + ($('#qty_'+i).val() * 1);
+		}
+	}
+	
+	$("#totalqty").html(totalQTY);
 }
